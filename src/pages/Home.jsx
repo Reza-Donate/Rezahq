82b1a98709e9c4fe5e876e3bd469772e9be09ea7
Kaodily <isizohchika@gmail.com>
2023-08-25 01:12:26 +0100

    refactored

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,14 +4,16 @@ import Hero from "../components/Hero";
 import FormField from "../components/FormField";
 import MobileMenu from "../components/MobileMenu";
 import Services from "../components/Services";
+import Footer from "../components/Footer"
 const Home = () => {
   return (
-    <div className="pb-6">
+    <div className="">
       <Header />
       <MobileMenu />
       <Hero />
       <Services/>
       <FormField />
+      <Footer />
     </div>
   );
 };
